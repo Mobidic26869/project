5d75da2d285dad4d05e008e5583f625fa285f2ed
Mobidic26869 <59160114@go.buu.ac.th>
2019-10-15 01:52:22 +0700

    title go to category

--- app/src/main/java/buu/lucio/thaiproverbs/Title.kt
+++ app/src/main/java/buu/lucio/thaiproverbs/Title.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import buu.lucio.thaiproverbs.databinding.FragmentTitleBinding
 
 /**
@@ -17,6 +18,10 @@ class Title : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                                 savedInstanceState: Bundle?): View? {
        val binding = DataBindingUtil.inflate<FragmentTitleBinding>(inflater,R.layout.fragment_title,container,false)
+        binding.categoryButton.setOnClickListener {view ->
+            view.findNavController().navigate(R.id.action_title2_to_category2)
+
+        }
         return binding.root
     }
 
